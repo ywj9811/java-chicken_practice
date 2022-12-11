ad96005e0206ae611a0c3a6100770d580fac6371
양원준 <97458548+ywj9811@users.noreply.github.com>
2022-12-11 22:22:33 +0900

    feat: 테이블마다 메뉴에 대한 정보 추가

--- src/main/java/domain/TablePrice.java
+++ src/main/java/domain/TablePrice.java
@@ -1,20 +1,27 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TablePrice {
     private final int table;
-    private int price;
 
+    private final List<Menu> menus = MenuRepository.menus();
 
-    public TablePrice(int table, int price) {
-        this.price = price;
+    public TablePrice(int table) {
         this.table = table;
     }
-
-    public int getPrice() {
-        return price;
+    public List<Menu> getMenus() {
+        return menus;
     }
 
-    public void setPrice(int plusPrice) {
-        price += plusPrice;
+    public void setMenuQuantity(int menuNumber, int quantity) {
+        if (menuNumber < 7) {
+            menus.get(menuNumber - 1).setQuantity(quantity);
+            return;
+        }
+        menus.get(menuNumber - 15).setQuantity(quantity);
+        return;
     }
+
 }
