f9b6292c42b89afe7783c31302e4de93b8cff751
양원준 <97458548+ywj9811@users.noreply.github.com>
2022-12-11 22:22:56 +0900

    fix: 테이블 마다의 정보 도메인에 대해 가격 삭제

--- src/main/java/domain/TablePriceRepository.java
+++ src/main/java/domain/TablePriceRepository.java
@@ -7,13 +7,13 @@ public class TablePriceRepository {
     private static final List<TablePrice> tablePrices = new ArrayList<>();
 
     static {
-        tablePrices.add(new TablePrice(1, 0));
-        tablePrices.add(new TablePrice(2, 0));
-        tablePrices.add(new TablePrice(3, 0));
-        tablePrices.add(new TablePrice(4, 0));
-        tablePrices.add(new TablePrice(5, 0));
-        tablePrices.add(new TablePrice(6, 0));
-        tablePrices.add(new TablePrice(8, 0));
+        tablePrices.add(new TablePrice(1));
+        tablePrices.add(new TablePrice(2));
+        tablePrices.add(new TablePrice(3));
+        tablePrices.add(new TablePrice(4));
+        tablePrices.add(new TablePrice(5));
+        tablePrices.add(new TablePrice(6));
+        tablePrices.add(new TablePrice(8));
     }
 
     public static List<TablePrice> getTablePrices() {
