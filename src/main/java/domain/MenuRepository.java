184ea8068164b199b93e5dd57187550e498180de
양원준 <97458548+ywj9811@users.noreply.github.com>
2022-12-11 23:19:06 +0900

    fix: static에 의한 객체 초기화 문제 해결

--- src/main/java/domain/MenuRepository.java
+++ src/main/java/domain/MenuRepository.java
@@ -5,10 +5,10 @@ import java.util.Collections;
 import java.util.List;
 
 public class MenuRepository {
-    private static final List<Menu> menus = new ArrayList<>();
+    private List<Menu> menus = new ArrayList<>();
 
-    static {
-        menus.add(new Menu(1, "후라이드", Category.CHICKEN, 16_000));
+    public MenuRepository() {
+        menus.add(new Menu(1, "후라이드",Category.CHICKEN, 16_000));
         menus.add(new Menu(2, "양념치킨", Category.CHICKEN, 16_000));
         menus.add(new Menu(3, "반반치킨", Category.CHICKEN, 16_000));
         menus.add(new Menu(4, "통구이", Category.CHICKEN, 16_000));
@@ -18,7 +18,7 @@ public class MenuRepository {
         menus.add(new Menu(22, "사이다", Category.BEVERAGE, 1_000));
     }
 
-    public static List<Menu> menus() {
+    public List<Menu> menus() {
         return Collections.unmodifiableList(menus);
     }
 }
