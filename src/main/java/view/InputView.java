c28ac1098f1aa9c7854ad753cf61a35038f23730
양원준 <97458548+ywj9811@users.noreply.github.com>
2022-12-11 21:47:41 +0900

    feat: 테이블 입력 및 메뉴 입력 기능

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,8 +5,23 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
+    public static int wantFunction() {
+        System.out.println("## 원하는 기능을 선택하세요.");
+        return scanner.nextInt();
+    }
+
     public static int inputTableNumber() {
         System.out.println("## 주문할 테이블을 선택하세요.");
+        return scanner.nextInt();
+    }
+    
+    public static int inputMenuNumber() {
+        System.out.println("## 등록할 메뉴를 선택하세요.");
+        return scanner.nextInt();
+    }
+    
+    public static int menuCount() {
+        System.out.println("## 메뉴의 수량을 입력하세요.");
         return scanner.nextInt();
     }
 }
